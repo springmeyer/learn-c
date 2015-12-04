ca1ebba36a5f0cca94cb255c21b98cd3cd040488
karenzshea <karenzshea@gmail.com>
2015-12-04 16:47:35 -0500

    linux special fun

--- step_1c/run.c
+++ step_1c/run.c
@@ -3,19 +3,22 @@
 /*
 
 # build "implementation" object file
+## OS X
 clang -c implementation.c -I./
+## Linux
+clang -c implementation.c -I./ -fPIC
 
 # build "implementation" library
 ## OS X
 clang -dynamiclib -o liblearn.dylib implementation.o
 ## Linux
-clang -dynamiclib -o liblearn.so implementation.o
+clang -shared -o liblearn.so implementation.o
 
 # compile run.c
 clang -c run.c -I./
 
 # link run program
-clang -o run run.o -L./ -llearn
+clang -o run run.o -L./ -llearn -Wl,-z,origin -Wl,-rpath=\$ORIGIN
 
 */
 
