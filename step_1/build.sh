601693bc321602cd98ca48d26cd27d013d0cfd9d
Dane Springmeyer <dane@mapbox.com>
2016-01-13 14:43:30 -0800

    add note about how to compile, link, and run in one line

--- step_1/build.sh
+++ step_1/build.sh
@@ -8,3 +8,15 @@ clang -c run.c
 
 # link into executable in second step
 clang -o run run.o
+
+: '
+
+NOTE: for fast iteration you might want to avoid using a bash wrapper and just
+have a command that could go on one line and be re-run.
+
+To do that you could compile, link, and run the program in one line like:
+
+
+   clang -o run run.c && ./run
+
+'
